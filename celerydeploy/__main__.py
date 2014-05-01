c122308d95b94620f2453f7b7f8c9f671b750588
Mher Movsisyan <mher@saucelabs.com>
2014-05-01 15:59:32 +0400

    Load local SSH config

--- celerydeploy/__main__.py
+++ celerydeploy/__main__.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import
 import os
 import sys
 
+from fabric.state import env
 from fabric.main import main as fabric_main
 
 import celerydeploy
 
+env.use_ssh_config = True
 
 def main():
     fabfile = os.path.join(celerydeploy.__path__[0], '__init__.py')
